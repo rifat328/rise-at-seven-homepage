9ec0e68369c09bc302615a3dd12bd96c0dbca3a6
Rifat Hossain <rifathossain328663@gmail.com>
2026-05-06 11:39:49 +0600

    main project start : page refactor

---
+++ rise-at-seven/components/testPage.js
@@ -0,0 +1,89 @@
+"use client";
+import CollapsibleList from "@/components/CollapsibleList";
+import DismissableAlert from "@/components/DismissableAlert";
+import LiveBadge from "@/components/LiveBadge";
+import { motion } from "motion/react";
+
+export default function Home() {
+  const feature = ["Fast", "Slow", "PowerFull", "Weak"];
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2, ease: "easeIn" },
+    },
+  };
+  const items = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+  };
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        <h1 className="text-4xl font-bold text-center">Hello World</h1>
+        <div className="flex gap-3 mt-4">
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 300, damping: 10 }}
+            className="text-sm text-gray-500 border ring-1 rounded-2xl px-3 py-1"
+          >
+            Spring animation
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "tween" }}
+            className="text-sm text-gray-500 border ring-1 rounded-2xl px-3 py-1"
+          >
+            Tween animation
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "tween" }}
+            className="text-sm text-gray-500 border ring-1 rounded-2xl px-3 py-1"
+          >
+            Inertia animation
+          </motion.button>
+        </div>
+        <div className="py-4 px-3 space-y-5 mt-5">
+          <motion.ul
+            variants={container}
+            initial={"hidden"}
+            animate={"visible"}
+          >
+            {feature.map((e) => (
+              <motion.li
+                key={e}
+                variants={items}
+                className="text-shadow-white font-medium"
+              >
+                {e}
+              </motion.li>
+            ))}
+          </motion.ul>
+        </div>
+        <LiveBadge />
+        <motion.div
+          className="py-2 px-3  mt-5"
+          drag
+          dragConstraints={{ left: -80, right: 80, top: -40, bottom: 40 }}
+          dragElastic={0.2}
+        >
+          <h3 className="p-2 rounded-2xl shadow-2xl">Abu Bakkar</h3>
+          <h3 className="p-2 rounded-2xl shadow-2xl">Umar ibn </h3>
+          <h3 className="p-2 rounded-2xl shadow-2xl">Hamza</h3>
+          <h3 className="p-2 rounded-2xl shadow-2xl">Ali</h3>
+        </motion.div>
+
+        <DismissableAlert />
+        <CollapsibleList />
+      </motion.div>
+    </div>
+  );
+}
