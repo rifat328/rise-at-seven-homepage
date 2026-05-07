3b17e29e09a2b0de5316d243824baeef80182086
Rifat Hossain <rifathossain328663@gmail.com>
2026-05-07 22:36:04 +0600

    Update: updated Header for mobile accordian content & styles

--- rise-at-seven/app/page.js
+++ rise-at-seven/app/page.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen">
+    <main className="font-saans min-h-screen">
       <Header />
       <h1 className="font-saans text-4xl font-medium leading-none text-foreground mt-80">
         Services
