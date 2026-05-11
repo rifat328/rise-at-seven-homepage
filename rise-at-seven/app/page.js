e0590a99b38a357e76e8b7fc79e4d9914b71cfaa
Rifat Hossain <rifathossain328663@gmail.com>
2026-05-11 14:00:12 +0600

    Feat: Enhance Hero section with new content, styling updates, and add Leaf components; update navigation items

--- rise-at-seven/app/page.js
+++ rise-at-seven/app/page.js
@@ -2,14 +2,16 @@
 import Button from "@/components/Button";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="font-saans min-h-screen">
-      <div className="relative  h-14  m-2.5">
-        <Button classStyle="  w-full bg-[#AAF0D1] text-grey-900 py-2 px-5 text-sm font-semibold  text-balance  tracking-tight leading-none font-semibold  | lg:text-sm |">
+    <main className="font-saans min-h-screen flex flex-col">
+      <div className="relative h-3.5 mx-2.5 mt-2.5 mb-4 shrink-0">
+        <Button classStyle="w-full bg-[#AAF0D1] text-grey-900 py-2 px-5 text-sm font-semibold text-balance tracking-tight leading-none | lg:text-sm |">
           🚨 The Category Leaderboard - Live Now
         </Button>
       </div>
+
       <Header />
 
       {/* Hero Content */}
--- rise-at-seven/components/Hero.js
+++ rise-at-seven/components/Hero.js
@@ -1,32 +1,109 @@
 import React from "react";
-
+import Image from "next/image";
+import LeafLeft from "./LeafLeft";
+import LeafRight from "./LeafRight";
+import { heroIconsBottom } from "@/utils/navItems";
 const Hero = () => {
+  const heroFrontIconWidth = 35,
+    heroFrontIconHeight = 20;
+
   return (
     <section
       aria-label="Hero section, top of the page"
-      className="w-screen h-screen rounded-md p-2"
+      className="w-full h-[calc(100vh-66px)] p-2.5 flex flex-col"
     >
-      {/* hero content */}
-      <div className="main">
-        <p>#1 Most recommended content marketing agency</p>
-        <div class="branding-icons">
-          <img src="null" alt="" />
+      <div className="main-hero bg-amber-500 w-full h-full rounded-2xl p-3 md:p-4 flex flex-col relative">
+        {/* --- TOP / CENTER CONTENT --- */}
+        <div className="font-saans font-normal text-background px-8 flex-1 flex flex-col justify-center items-center text-center">
+          <p className="font-medium text-sm leading-3.5 max-w-52 mb-1">
+            #1 Most recommended content marketing agency
+          </p>
+
+          <div className="branding-icons my-4 flex gap-3 ">
+            <LeafLeft />
+            <Image
+              src="/image/Hero/heroFrontTopMiddleIcon/HeroFront-TopMiddleIcon-1.webp"
+              alt="Global Search Awards"
+              width={heroFrontIconWidth}
+              height={heroFrontIconHeight}
+              quality={100}
+            />
+            <Image
+              src="/image/Hero/heroFrontTopMiddleIcon/HeroFront-TopMiddleIcon-2.webp"
+              alt="The Drum"
+              width={heroFrontIconWidth}
+              height={heroFrontIconHeight}
+              quality={100}
+            />
+            <Image
+              src="/image/Hero/heroFrontTopMiddleIcon/HeroFront-TopMiddleIcon-3.webp"
+              alt="UK social Media Awards"
+              width={heroFrontIconWidth}
+              height={heroFrontIconHeight}
+              quality={100}
+            />
+            <Image
+              src="/image/Hero/heroFrontTopMiddleIcon/HeroFront-TopMiddleIcon-4.webp"
+              alt="UK Content Awards"
+              width={heroFrontIconWidth}
+              height={heroFrontIconHeight}
+              quality={100}
+            />
+            <LeafRight />
+          </div>
+
+          <div className="main-text-content">
+            <span className="block text-5xl md:text-7xl 2xl:text-9xl font-medium tracking-none leading-none">
+              We Create Category
+            </span>
+            {/* <img src="null" alt="" className="mx-auto my-4" /> */}
+            <Image
+              src="/image/Hero/heroFrontTopMiddleIcon/HeroFront-TopMiddleIcon-1.webp"
+              alt="Global Search Awards"
+              width={heroFrontIconWidth}
+              height={heroFrontIconHeight}
+              quality={100}
+            />
+            <span className="block text-5xl md:text-7xl font-medium 2xl:text-9xl  tracking-none leading-none mt-2">
+              Leaders
+            </span>
+            <p className="text-2xl md:text-2xl mt-6 font-semibold">
+              on every searchable platform
+            </p>
+          </div>
+          <div className="on-4k-icons hidden 2xl:flex gap-3 justify-center items-center">
+            {heroIconsBottom.map((item, index) => (
+              <Image
+                key={index}
+                src={item.src}
+                alt={item.alt}
+                width={item.width}
+                height={item.height}
+                quality={item.quality}
+                placeholder={item.placeholder}
+                blurDataURL={item.shimmer}
+              />
+            ))}
+          </div>
         </div>
-        <div class="main-text-content">
-          <span>W e C r e a t e C a t e g o r y</span>
-          <img src="null" alt="" />
-          <span>L e a d e r s</span>
-          <p>on every searchable platform</p>
+
+        {/* --- BOTTOM SUB-TEXT SECTION --- */}
+        <div className="w-full text-white text-sm md:text-base font-normal mt-auto">
+          <div className="sub-content flex flex-col md:flex-row md:justify-between w-full md:gap-12">
+            <p className="hidden md:block text-left  md:text-xs leading-snug md:max-w-[50%] text-amber-50">
+              Organic media planners creating, distributing & optimising{" "}
+              <span className="font-semibold">search-first</span> content for
+              SEO, Social, PR, Ai and LLM search
+            </p>
+
+            <p className="md:flex md:flex-row md:justify-end md:items-baseline md:gap-1.5 mt-4 md:mt-0 font-medium text-center md:text-right text-white">
+              <span className="whitespace-nowrap">
+                4 Global Offices serving{" "}
+              </span>
+              <span className="whitespace-nowrap">UK, USA (New York) & EU</span>
+            </p>
+          </div>
         </div>
-      </div>
-      {/* sub-detail-text */}
-      <div className="sub-content flex justify-between">
-        <p>
-          Organic media planners creating, distributing & optimising
-          <span>search-first</span> content for SEO, Social, PR, Ai and LLM
-          search
-        </p>
-        <p>4 Global Offices serving UK, USA (New York) & EU</p>
       </div>
     </section>
   );
