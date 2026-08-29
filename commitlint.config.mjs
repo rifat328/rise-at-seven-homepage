8ebba1062170433f28c66f9037d9fb658ab93c21
Rifat Hossain <rifathossain328663@gmail.com>
2026-08-30 02:47:45 +0600

    fix(CommitLint): allow capitalized scopes in commit messages

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,8 @@
 const commitLint = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Allow capitalized scopes, e.g. "feat(Auth): authentication implemented"
+    "scope-case": [0],
+  },
 };
 export default commitLint;
